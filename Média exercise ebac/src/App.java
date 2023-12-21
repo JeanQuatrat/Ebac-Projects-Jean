1cb64acf704c7aaf8ba4cf1d35e370f5c2fbf7ec
JeanQuatrat <154040429+JeanQuatrat@users.noreply.github.com>
2023-12-21 16:44:28 -0300

    App.java

--- Média exercise ebac/src/App.java
+++ Média exercise ebac/src/App.java
@@ -6,7 +6,7 @@ public class App {
         int NotaPort = 9;
         int NotaArt = 10;
 
-        int NotaMedia = (5 + 8 + 9 + 10)/2;
+        int NotaMedia = (5 + 8 + 9 + 10)/4;
         //Retirei as acentuações por questão de embelezamento no terminal.
         System.out.format("Juntando as notas de Matematica %d, Historia %d, Portugues %d e Artes %d. Sua media final e de %d.", NotaMat, NotaHist, NotaPort, NotaArt, NotaMedia);
         
