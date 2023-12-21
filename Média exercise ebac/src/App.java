3644a85fb57dd0a5a418cb203e7464c23aa4326e
JeanQuatrat <154040429+JeanQuatrat@users.noreply.github.com>
2023-12-21 04:46:47 -0300

    Exercício Mód8 Ebac "calculo Média"

---
+++ Média exercise ebac/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int NotaMat = 5;
+        int NotaHist = 8;
+        int NotaPort = 9;
+        int NotaArt = 10;
+
+        int NotaMedia = (5 + 8 + 9 + 10)/2;
+        //Retirei as acentuações por questão de embelezamento no terminal.
+        System.out.format("Juntando as notas de Matematica %d, Historia %d, Portugues %d e Artes %d. Sua media final e de %d.", NotaMat, NotaHist, NotaPort, NotaArt, NotaMedia);
+        
+    }
+}
