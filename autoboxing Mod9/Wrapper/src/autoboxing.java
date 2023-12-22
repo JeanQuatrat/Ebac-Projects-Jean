6c7ae1c59017f00a7185fe0c90b9bdfd8eef36de
JeanQuatrat <154040429+JeanQuatrat@users.noreply.github.com>
2023-12-22 06:25:17 -0300

    Wrapper Exercise Ebac mod9

---
+++ autoboxing Mod9/Wrapper/src/autoboxing.java
@@ -0,0 +1,15 @@
+public class autoboxing {
+    public static void main(String[] args) {
+        
+        //boxing
+        double z = 73.12;
+        Double zd = (z);
+        System.out.println(zd);
+
+        //unboxing
+        Double z2 = 98.53;
+        double zx = (z2);
+        System.out.println(zx);
+        
+    }
+ }
